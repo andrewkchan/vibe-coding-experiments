e554157650719d30d463ace4aee1e6d7578068ae
Andrew Chan <andrewkchan.akc@gmail.com>
2025-07-05 07:17:34 +0000

    syntax

--- crawler/start_cluster.py
+++ crawler/start_cluster.py
@@ -26,7 +26,7 @@ for shard_i in range(24):
       f"--seed-file top-1M-domain-shards/top-1M-domains-shard-{shard_i}.txt " + 
       f"--redis-port {6379 + shard_i} " +
       f"--email test@gmail.com --data-dir /mnt/data_{shard_i//2} " + 
-      f"--cpu-alloc-start={procs_per_pod * shard_i} --resume --fetcher-workers 6000 " + 
+      f"--cpu-alloc-start {procs_per_pod * shard_i} --resume --fetcher-workers 6000 " + 
       f"--num-fetcher-processes {fetchers_per_pod} --num-parser-processes {parsers_per_pod} " + 
       f"--seeded-urls-only 2>&1 " + 
       f"| tee -a logs/shard_{shard_i}.log " + 
